23af6065178027090dbe0f001e2e49c5b9187ed8
faywinne <faywinne@users.noreply.github.com>
2019-09-12 15:19:50 -0500

    Blockchain: method to start chain with first block, also fetch last block on chain

--- src/com/collabera/bccontract/model/Blockchain.java
+++ src/com/collabera/bccontract/model/Blockchain.java
@@ -1,6 +1,32 @@
 package com.collabera.bccontract.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Blockchain
 {
+	List<Block> blocks;
+	public static final String PUBLIC = "$nullkey$";
 	
+	public Blockchain()
+	{
+		blocks = new ArrayList<Block>();
+	}
+	
+	public Block buildFirstBlock(byte[] data)
+	{
+		//first block has no prior block's hash
+		//placeholder public key because data will not be RSA encrypted as other data would
+		//data is expected to hold information such as user list to be publicly available
+		blocks = new ArrayList<Block>();
+		Block block=new Block(0,0,data,PUBLIC);
+		blocks.add(block);
+		
+		return block;
+	}
+	
+	public Block getLast()
+	{
+		return blocks.get(blocks.size()-1);
+	}
 }
